d8c67dae4f78aa30e885ea8a3f46a60a6a079cda
wildanindi <wildanindi2@gmail.com>
2024-04-23 18:22:24 +0700

    add display dan prosedurnya

--- ClassBangunDatar.cpp
+++ ClassBangunDatar.cpp
@@ -19,5 +19,11 @@ class bangunDatar{
         float hitungLuas (){
             return panjang * Lebar;
         }
-        
+
+        void display() {
+            cout << "Panjangnya = " << panjang << endl;
+            cout << "Lebarnya = " << Lebar << endl;
+            cout << "Luasnya = " << hitungLuas() << endl;
+
+        }
 };
